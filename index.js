830d1a627dee42ea2c6e54e97b1777ae721694ce
Mvb1122 <micahbushman.school@gmail.com>
2024-02-04 13:06:16 -0700

    Extract model to fake interface
    I don't like JS classes so this is the best you're getting

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const model_id = "Xenova/TinyLlama-1.1B-Chat-v1.0";
-const ModelAssistantStart = "<|assistant|>";
-const Default_Settings = {
-    max_new_tokens: 256,
-    temperature: 0.8,
-    do_sample: true,
-    top_k: 50,
-};
+const model_id = "Felladrin-onnx-gpt2-large-conversational-retrain";
 
+/**
+ * @type {{ 
+ * model_id: String, 
+ * ModelAssistantStart: String, 
+ * Default_Settings: {{ max_new_tokens: number, temperature: number, do_sample: boolean, top_k: number}}, 
+ * Run: (property), 
+ * preload: (property), 
+ * }}
+ */
+const ModelHelper = require(`./Models/${model_id}`)
 
-const { pipeline } = import("@xenova/transformers");
+const SystemInfo = "You are a helpful assistant. Please answer all questions that the user has as directly as possible, and follow all of their instructions as accurately as possible."
+const UserInfo = "The user is Micah, a cute girl, whose pronouns are she/her."
 
+const ModelAssistantStart = ModelHelper.ModelAssistantStart;
+const Default_Settings = ModelHelper.Default_Settings;
+const Run = ModelHelper.Run;
+const preload = ModelHelper.preload;
 
 /**
  * @param {"system" | "user" | "assistant"} role 
@@ -20,91 +28,6 @@ function message(role, content) {
     return { role: role, content: content }
 }
 
-let tokenizer, model = tokenizer = null;
-/**
- * 
- * @param {[{role: String, content: String}]} messages 
- * @param {String} txt Text to use if no messages provided; uses default system prompt.
- * @returns {Promise<[{generated_text: String}]>} Generated result.
-*/
-async function Run(messages = null, txt = null, settings = null) {
-    await preload();
-
-    if (model == null)
-        model = await pipeline('text-generation', model_id);
-    /*
-    if (model == null)
-        model = await AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=true)
-
-    if (tokenizer == null)
-        tokenizer = await AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=true)
-
-    const inputs = tokenizer("Hi, how're you doing?")
-
-    let out = model.generate(inputs, max_length=200);
-    console.log(out);
-    // [{'label': 'POSITIVE', 'score': 0.999817686}]
-    */
-    return new Promise(async (resolve) => {
-
-        if (txt != null && messages == null) {
-            messages = [
-                { "role": "system", "content": "You are a friendly assistant." },
-                { "role": "user", "content": txt },
-            ]
-        }
-
-        // Construct the prompt
-        const prompt = model.tokenizer.apply_chat_template(messages, {
-            tokenize: false, add_generation_prompt: true,
-        });
-
-        // Generate a response
-        if (settings == null) {
-            settings = Default_Settings
-        }
-        const result = await model(prompt, settings);
-
-        resolve(result);
-    })
-
-    /*
-    const output = await model(txt, {
-        temperature: 2,
-        max_new_tokens: 100,
-        repetition_penalty: 1.5
-    });
-    console.log(output);
-    */
-};
-
-// Temp Test:
-async function TempTest() {
-    let sumOutput = "";
-    for (let i = 0.1; i <= 2; i += 0.1) {
-        const settings = Default_Settings;
-        settings.temperature = i;
-        
-        const result = await Run(null, "What do you think of Japan?", null, settings);
-        let LatestMessage = result[0].generated_text;
-        LatestMessage = LatestMessage.substring(LatestMessage.lastIndexOf(ModelAssistantStart) + ModelAssistantStart.length + 1)
-        const output = `\n# Temp: ${settings.temperature}\n${LatestMessage}`;
-        sumOutput += output;
-        console.log(output);
-    }
-    fs.writeFile("TempTest.md", sumOutput, (e) => {if (e) console.log(e)});
-}
-// TempTest();
-
-async function preload() {
-    return new Promise(async res => {
-        const { pipeline } = await import("@xenova/transformers");
-        if (model == null)
-            model = await pipeline('text-generation', model_id);
-
-        res();
-    })
-}
 
 const fs = require('fs');
 async function ActiveSession() {
@@ -117,8 +40,8 @@ async function ActiveSession() {
     
     // const testFile = fs.readFileSync("Test.md").toString();
     let messages = [
-        message("system", "You are a helpful assistant. Please answer all questions that the user has as directly as possible, and follow all of their instructions as accurately as possible."),
-        message("system", "The user is Micah, a cute girl, whose pronouns are she/her.")
+        message("system", SystemInfo),
+        message("system", UserInfo)
         /* message("system", "Given text: \n" + testFile) */
     ]
 
